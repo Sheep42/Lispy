5cbca9c95e9afec1976c49d3c8092dbb048468f8
dshedd <dan@dshedd.com>
2017-04-01 01:21:37 -0400

    Added PreProcessor rules for WIN32

--- main.c
+++ main.c
@@ -1,10 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* If compiling on windows use these */
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+/* fake readline function */
+char* readline(char* prompt) {
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+
+    char* cpy = malloc(strlen(buffer) + 1);
+    strcpy(cpy, buffer);
+    cpy[strlen(cpy) - 1] = '\0';
+
+    return cpy;
+}
+
+void add_history(char* unused) {}
+
+/* Otherwise */
+#else
 #include <editline/readline.h>
 #include <editline/history.h>
-
-static char input[2048];
+#endif
 
 int main(int argc, char** argv) {
 
