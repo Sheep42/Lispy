5d9ed9803bd08504e0821359fd0efd0de5e467dd
dshedd <dan@dshedd.com>
2017-04-01 12:58:24 -0400

    Installed MPC and modified interpreter to actually parse user input

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "mpc.h"
 
 /* If compiling on windows use these */
 #ifdef _WIN32
@@ -28,6 +29,22 @@ void add_history(char* unused) {}
 #endif
 
 int main(int argc, char** argv) {
+    /* Create some parsers */
+    mpc_parser_t* Number = mpc_new("number");
+    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Expr = mpc_new("expr");
+    mpc_parser_t* Lispy = mpc_new("lispy");
+
+    /* Define the parsers */
+    mpca_lang(MPCA_LANG_DEFAULT,
+        "                                                       \
+            number   :  /-?[0-9]+/ ;                            \
+            operator :  '+' | '-' | '*' | '/' ;                 \
+            expr     :  <number> | '(' <operator> <expr>+ ')' ; \
+            lispy    :  /^/ <operator> <expr>+ /$/ ;            \
+        ",
+        Number, Operator, Expr, Lispy
+    );
 
     /* Print Version and Exit info */
     puts("Lispy Version 0.0.0.0.1");
@@ -41,12 +58,25 @@ int main(int argc, char** argv) {
         /* Add input to history */
         add_history(input);
 
-        /* Echo input back to user */
-        printf("No, you're a %s\n", input);
+        /* Attempt to parse user input */
+        mpc_result_t r;
+
+        if(mpc_parse("<stdin>", input, Lispy, &r)) {
+            /* On success print the AST */
+            mpc_ast_print(r.output);
+            mpc_ast_delete(r.output);
+        } else {
+            /* Otherwise print the error */
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
 
         /* Free retrieved input */
         free(input);
     }
+
+    /* Clean up the parsers */
+    mpc_cleanup(4, Number, Operator, Expr, Lispy);
 
     return 0;
 }
